3d63ca978dfb1a1fd26e39ac114502ae1ba8755f
Sean Worthington <sean@worthington.net>
2020-08-03 15:20:09 -0700

    Update action.php

--- sample/action.php
+++ sample/action.php
@@ -18,7 +18,7 @@
 	
 	
 /* 2. Check that this order has not been processed before */
-	// $past_order_count = SELECT COUNT(*) FROM orders WHERE order_id = $$receipt_guid
+	// $past_order_count = SELECT COUNT(*) FROM orders WHERE order_id = $receipt_guid
 			$past_order_count = 0;
 	if( $past_order_count > 0 ) {
 		echo("This order has already been processed.");
